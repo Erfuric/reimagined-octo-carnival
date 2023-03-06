167fbf7de0ad44a43b9ebd786d4ce5433e58f988
Alyshia Kandler <a.lyshia@hotmail.com>
2023-03-06 21:23:29 +1100

    genre model started

---
+++ Develop/models/Genre.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Genre extends Model {}
+
+Genre.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      notNull: true
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'genre',
+  }
+);
+
+module.exports = Genre;
