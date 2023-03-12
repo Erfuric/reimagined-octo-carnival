d2d3febb6fedec5a53026b66ecf8c7d2139492e3
NealeAust <nealechapman@optushome.com.au>
2023-03-12 16:39:36 +1100

    updated login hdl and added signup hdl

--- Develop/public/js/signup.js
+++ Develop/public/js/signup.js
@@ -32,23 +32,3 @@ async function signupFormHandler(event) {
 document
     .querySelector('signup-form')
     .addEventListener('submit', signupFormHandler);
-
-
-
-
-
-
-
-
-})
-
-
-
-
-
-
-
-
-
-
-}
