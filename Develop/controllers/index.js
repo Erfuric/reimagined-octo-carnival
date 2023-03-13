2466d8e0155579e1191fc37c8f5335567c6e2f49
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-03-13 18:32:23 +1100

    uncommented lines in controllers index

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,12 +1,12 @@
 //main controller to direct routes
 
 const router = require('express').Router();
-// const apiRoutes = require('./api');
+const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
 
 router.use('/', homeRoutes);
-// router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
 
 
 
