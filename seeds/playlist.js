64fb2664263205e2023cfaa77b6fa05fb2c6143a
Alyshia Kandler <alyshiakandler@Alyshias-MacBook-Air.local>
2023-03-13 21:18:26 +1100

    directory mapping fixed

--- seeds/playlist.js
+++ seeds/playlist.js
@@ -2,26 +2,32 @@ const { Playlist } = require('../models');
 
 const playlistData = [
     {
+        id: 1,
         playlist_name: "NealeAust",
         user_id: 2,
     },
     {
+        id: 2,
         playlist_name: "Rockin",
         user_id: 8,
     },
     {
+        id: 3,
         playlist_name: "Country_Songs",
         user_id: 13,
     },
     {
+        id: 4,
         playlist_name: "Mix",
         user_id: 14,
     },
     {
+        id: 5,
         playlist_name: "Moondust",
         user_id: 20,
     },
     {
+        id: 6,
         playlist_name: "Tornado99",
         user_id: 21,
     },
