d1b95048a0b055563c56a793d21e12eb761e0b7f
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-03-14 21:29:43 +1100

    Added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,20 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
       host: 'localhost',
       dialect: 'mysql',
-      port: 8000, 
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
 
