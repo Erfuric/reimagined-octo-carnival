86435056f2965202da18fda3351b63bb280d2a53
skwidhubz <tommaxwellparker@gmail.com>
2023-03-15 20:10:42 +1100

    yeebs

--- public/js/login.js
+++ public/js/login.js
@@ -1,31 +1,54 @@
-//  Login form handler.
-async function loginFormHandler(event) {
+const submitButton = document.getElementById('submit-button');
+
+
+
+function loginFunction (event) {
     event.preventDefault();
 
-    console.log('login handler function engaged');
+    console.log('login function executed');
 
-    // Get information entered in login form.
-    const email = document.querySelector('#email-login').value.trim();
+    const user = document.querySelector('#user-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
 
-    // Upon entering login details.
-    if (password) {
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify({
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' },
-        });
+    console.log(user) && console.log(password);
 
-        // Response to fetch received, status checked and applicable message displayed. 
-        if (response.ok) {
-            document.location.replace('/');
-            alert('Welcome to MixMate!');
-        } else {
-            alert('Failed to log in, please try again!');
-        }
-    }
-};
+}
 
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+submitButton.addEventListener('click', loginFunction)
+
+
+
+
+                // //  Login form handler.
+                // async function loginFormHandler(event) {
+                //     event.preventDefault();
+
+                //     console.log('login handler function engaged');
+
+                //     // Get information entered in login form.
+                //     const user = document.querySelector('#user-login').value.trim();
+                //     const password = document.querySelector('#password-login').value.trim();
+
+                //     // Upon entering login details.
+                //     if (password) {
+                //         const response = await fetch('/api/users/login', {
+                //             method: 'POST',
+                //             body: JSON.stringify({
+                //                 password
+                //             }),
+                //             headers: { 'Content-Type': 'application/json' },
+                //         });
+
+                //         // Response to fetch received, status checked and applicable message displayed. 
+                //         if (response.ok) {
+                //             document.location.replace('/');
+                //             alert('Welcome to MixMate!');
+                //         } else {
+                //             alert('Failed to log in, please try again!');
+                //         }
+                //     }
+                // };
+
+
+
