cd8160eaeb8fed85858e6f01281281ed82cd9085
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-03-22 15:04:44 +1100

    Blororo

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ const loginFormHandler = async (event) => {
   const password = document.querySelector('#password-login').value.trim();
 
   if (username && password) {
-    const response = await fetch('/login', {
+    const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ name: username, password }),
       headers: { 'Content-Type': 'application/json' },
