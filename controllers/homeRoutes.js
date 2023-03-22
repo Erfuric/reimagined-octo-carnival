2d2a9773a5de7eeb569aa9bca049d668f538bf23
skwidhubz <tommaxwellparker@gmail.com>
2023-03-22 19:54:28 +1100

    backend receive from front end createplaylist

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,59 +1,79 @@
 const router = require('express').Router();
-const { User, Song } = require('../models');
+const { User } = require('../models');
 const withAuth = require('../utils/auth');
 const path = require('path');
 const Playlist = require('../models/playlist');
 
 router.get('/', withAuth, async (req, res) => {
   try {
-    const playlistData = await Playlist.findAll({
-      include: [{ model: Song, User }],
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['username', 'ASC']],
     });
-    const playlistAll = playlistData.map((obj) => obj.get({ plain: true }));
-    res.render('playlist', { playlistAll, logged_in: req.session.logged_in });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+
+
+    res.render('homepage', {
+      users,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
-    console.log(err);
+    console.log(err)
     res.status(500).json(err);
   }
 });
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/playlist');
+    res.redirect('/');
     return;
   }
 
   res.render('login');
 });
 
-router.get('/logout', async (req, res) => {
+
+router.post('/logout', (req, res) => {
   console.log('Logout route called');
   if (req.session.logged_in) {
-    req.session.destroy((err) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({ message: 'Error during logout' });
-      }
-      res.clearCookie('connect.sid');
-      // Redirect to the login page after successfully logging out
+    req.session.destroy(() => {
       res.redirect('/login');
     });
   } else {
-    res.status(400).json({ message: 'You are not logged in' });
+    res.redirect('/login');
   }
 });
 
 router.get('/playlist', async (req, res) => {
-  const playlistData = await Playlist.findAll({
-    include: [{ model: Song, User }],
-  });
-  const playlistAll = playlistData.map((obj) => obj.get({ plain: true }));
-  res.render('playlist', { playlistAll });
+  // const playlistData = await Playlist.findAll({
+  //   // include: [{ model: Song, User },],
+  // });
+  // const playlistAll = playlistData.map(obj => obj.get({plain: true}))
+  // res.render('playlist', { playlistAll })
+  res.render('playlist');
+
 });
 
-// Send newplaylist {{template}} on navbar click
+// send newplaylist {{template}} on navbar click
 router.get('/newplaylist', async (req, res) => {
   res.render('newplaylist');
+})
+
+
+// post handle from cr8playlist button
+router.post('/newplaylist', async (req, res) => {
+  try {
+    const newPlaylist = await Playlist.create({
+      ...req.body,
+      creator: req.session.user_id,
+    });
+    res.status(200).json(newPlaylist);
+  } catch (err) {
+    res.status(400).json(err);
+    console.log(err);
+  }
 });
+
 
 module.exports = router;
