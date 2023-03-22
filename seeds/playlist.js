d716e4f245c2ecf45adb99ad5527ea7b9fb1009a
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-03-22 19:20:13 +1100

    Lots of songs

--- seeds/playlist.js
+++ seeds/playlist.js
@@ -3,22 +3,52 @@ const { Playlist } = require('../models');
 const playlistData = [
     {
         id: 1,
-        playlist_name: "test list 1",
-        genre: "rock",
+        playlist_name: "Rock Classics",
+        genre: "Rock",
         user_id: 1,
     },
     {
         id: 2,
-        playlist_name: "test list 2",
-        genre: "pop",
+        playlist_name: "Hip Hop Jams",
+        genre: "Hip Hop",
         user_id: 2,
     },
     {
         id: 3,
-        playlist_name: "test list 3",
-        genre: "ligma",
+        playlist_name: "Country Favorites",
+        genre: "Country",
         user_id: 3,
-    }
+    },
+    {
+        id: 4,
+        playlist_name: "EDM Party",
+        genre: "Electronic Dance Music",
+        user_id: 1,
+    },
+    {
+        id: 5,
+        playlist_name: "Throwback 90s",
+        genre: "Pop",
+        user_id: 2,
+    },
+    {
+        id: 6,
+        playlist_name: "R&B Grooves",
+        genre: "R&B",
+        user_id: 3,
+    },
+    {
+        id: 7,
+        playlist_name: "Acoustic Chill",
+        genre: "Folk",
+        user_id: 1,
+    },
+    {
+        id: 8,
+        playlist_name: "Workout Pump-Up",
+        genre: "Pop",
+        user_id: 2,
+    },
 ];
 
 const seedPlaylists = () => Playlist.bulkCreate(playlistData);
--- seeds/songs.js
+++ seeds/songs.js
@@ -1,7 +1,6 @@
 const { Song } = require('../models');
 
 const songData = [
-
     {
         song: "song 1",
         artist: "artist 1",
@@ -15,7 +14,127 @@ const songData = [
     {
         song: "song 3",
         artist: "artist 3",
+        playlist_id: 1,
+    },
+    {
+        song: "song 4",
+        artist: "artist 4",
+        playlist_id: 1,
+    },
+    {
+        song: "song 5",
+        artist: "artist 5",
+        playlist_id: 1,
+    },
+    {
+        song: "song 6",
+        artist: "artist 6",
+        playlist_id: 1,
+    },
+    {
+        song: "song 7",
+        artist: "artist 7",
+        playlist_id: 1,
+    },
+    {
+        song: "song 8",
+        artist: "artist 8",
+        playlist_id: 1,
+    },
+    {
+        song: "song 1",
+        artist: "artist 1",
+        playlist_id: 2,
+    },
+    {
+        song: "song 2",
+        artist: "artist 2",
+        playlist_id: 2,
+    },
+    {
+        song: "song 3",
+        artist: "artist 3",
+        playlist_id: 2,
+    },
+    {
+        song: "song 4",
+        artist: "artist 4",
+        playlist_id: 2,
+    },
+    {
+        song: "song 5",
+        artist: "artist 5",
+        playlist_id: 2,
+    },
+    {
+        song: "song 6",
+        artist: "artist 6",
+        playlist_id: 2,
+    },
+    {
+        song: "song 7",
+        artist: "artist 7",
+        playlist_id: 2,
+    },
+    {
+        song: "song 8",
+        artist: "artist 8",
+        playlist_id: 2,
+    },
+    {
+        song: "song 1",
+        artist: "artist 1",
         playlist_id: 3,
+    },
+    {
+        song: "song 2",
+        artist: "artist 2",
+        playlist_id: 3,
+    },
+    {
+        song: "song 3",
+        artist: "artist 3",
+        playlist_id: 3,
+    },
+    {
+        song: "song 4",
+        artist: "artist 4",
+        playlist_id: 3,
+    },
+    {
+        song: "song 5",
+        artist: "artist 5",
+        playlist_id: 3,
+    },
+    {
+        song: "song 6",
+        artist: "artist 6",
+        playlist_id: 3,
+    },
+    {
+        song: "song 7",
+        artist: "artist 7",
+        playlist_id: 3,
+    },
+    {
+        song: "song 8",
+        artist: "artist 8",
+        playlist_id: 3,
+    },
+    {
+        song: "song 1",
+        artist: "artist 1",
+        playlist_id: 4,
+    },
+    {
+        song: "song 2",
+        artist: "artist 2",
+        playlist_id: 4,
+    },
+    {
+        song: "song 3",
+        artist: "artist 3",
+        playlist_id: 4,
     },
     {
         song: "song 4",
@@ -26,6 +145,181 @@ const songData = [
         song: "song 5",
         artist: "artist 5",
         playlist_id: 4,
+    },
+    {
+        song: "song 6",
+        artist: "artist 6",
+        playlist_id: 4,
+    },
+    {
+        song: "song 7",
+        artist: "artist 7",
+        playlist_id: 4,
+    },
+    {
+        song: "song 8",
+        artist: "artist 8",
+        playlist_id: 4,
+    },
+    {
+        song: "song 1",
+        artist: "artist 1",
+        playlist_id: 5,
+    },
+    {
+        song: "song 2",
+        artist: "artist 2",
+        playlist_id: 5,
+    },
+    {
+        song: "song 3",
+        artist: "artist 3",
+        playlist_id: 5,
+    },
+    {
+        song: "song 4",
+        artist: "artist 4",
+        playlist_id: 5,
+    },
+    {
+        song: "song 5",
+        artist: "artist 5",
+        playlist_id: 5,
+    },
+    {
+        song: "song 6",
+        artist: "artist 6",
+        playlist_id: 5,
+    },
+    {
+        song: "song 7",
+        artist: "artist 7",
+        playlist_id: 5,
+    },
+    {
+        song: "song 8",
+        artist: "artist 8",
+        playlist_id: 5,
+    },
+    {
+        song: "song 1",
+        artist: "artist 1",
+        playlist_id: 6,
+    },
+    {
+        song: "song 2",
+        artist: "artist 2",
+        playlist_id: 6,
+    },
+    {
+        song: "song 3",
+        artist: "artist 3",
+        playlist_id: 6,
+    },
+    {
+        song: "song 4",
+        artist: "artist 4",
+        playlist_id: 6,
+    },
+    {
+        song: "song 5",
+        artist: "artist 5",
+        playlist_id: 6,
+    },
+    {
+        song: "song 6",
+        artist: "artist 6",
+        playlist_id: 6,
+    },
+    {
+        song: "song 7",
+        artist: "artist 7",
+        playlist_id: 6,
+    },
+    {
+        song: "song 8",
+        artist: "artist 8",
+        playlist_id: 6,
+    },
+    {
+        song: "song 1",
+        artist: "artist 1",
+        playlist_id: 7,
+    },
+    {
+        song: "song 2",
+        artist: "artist 2",
+        playlist_id: 7,
+    },
+    {
+        song: "song 3",
+        artist: "artist 3",
+        playlist_id: 7,
+    },
+    {
+        song: "song 4",
+        artist: "artist 4",
+        playlist_id: 7,
+    },
+    {
+        song: "song 5",
+        artist: "artist 5",
+        playlist_id: 7,
+    },
+    {
+        song: "song 6",
+        artist: "artist 6",
+        playlist_id: 7,
+    },
+    {
+        song: "song 7",
+        artist: "artist 7",
+        playlist_id: 7,
+    },
+    {
+        song: "song 8",
+        artist: "artist 8",
+        playlist_id: 7,
+    },
+    {
+        song: "song 1",
+        artist: "artist 1",
+        playlist_id: 8,
+    },
+    {
+        song: "song 2",
+        artist: "artist 2",
+        playlist_id: 8,
+    },
+    {
+        song: "song 3",
+        artist: "artist 3",
+        playlist_id: 8,
+    },
+    {
+        song: "song 4",
+        artist: "artist 4",
+        playlist_id: 8,
+    },
+    {
+        song: "song 5",
+        artist: "artist 5",
+        playlist_id: 8,
+    },
+    {
+        song: "song 6",
+        artist: "artist 6",
+        playlist_id: 8,
+    },
+    {
+        song: "song 7",
+        artist: "artist 7",
+        playlist_id: 8,
+    },
+    {
+        song: "song 8",
+        artist: "artist 8",
+        playlist_id: 8,
     },
 ];
 
